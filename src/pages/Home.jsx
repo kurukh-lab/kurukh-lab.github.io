7b2c1418366de9f27787e9543710d4d3b43d35e4
Gaurav Kispotta <gaurav.ranchi1@gmail.com>
2025-05-24 17:22:05 +0530

    feat: Integrate Tailwind CSS and enhance Home page with card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,10 +75,25 @@ const Home = () => {
         <div className="w-full max-w-2xl">
           <WordList 
             words={searchResults} 
-            title="Search Results" 
+            title="Search Results 0" 
           />
         </div>
       )}
+
+<div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
 
       {/* Word of the Day Section */}
       {!loading && !error && searchResults.length === 0 && wordOfTheDay && (
