5f205c9aac4dda8e8de5adaf57c68bd7abbbac7f
Gaurav Kispotta <gaurav.ranchi1@gmail.com>
2025-05-25 00:12:21 +0530

    fix(Header): remove unnecessary z-index from user menu dropdown

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
               </div>
             </label>
             {isUserMenuOpen && (
-              <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+              <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
                 <li>
                   <Link to="/profile" className="justify-between" onClick={closeUserMenu}>
                     Profile
